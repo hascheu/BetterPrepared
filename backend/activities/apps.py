68fd7961cd738bf00ef5c5992515d6f0fa2d641d
hascheu <hannascheudeck@yahoo.de>
2026-05-15 09:38:29 +0200

    planner umbenannt

--- backend/activities/apps.py
+++ backend/activities/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
-
-class PlannerConfig(AppConfig):
-    name = 'planner'
+class ActivitiesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'activities'
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt', # Das JWT-Modul
 
     # Meine App
-    'planner',
+    'activities',
 ]
 
 # Konfiguriere das REST Framework
